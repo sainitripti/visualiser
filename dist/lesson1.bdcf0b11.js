10f87cd346dcd277a2661a0415225914f4b8f423
Saini <Tripti.Saini@philips.com>
2021-01-30 23:09:56 +0530

    Add lesson 4 and data files

--- dist/lesson1.bdcf0b11.js
+++ dist/lesson1.bdcf0b11.js
@@ -795,7 +795,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64704" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53290" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ src/js/lesson4.js
@@ -0,0 +1,66 @@
+import LessonOnePreviewCard from './LessonOnePreviewCard.js';
+
+var boxData = {
+    width: 50,
+    height: 50,
+    depth: 50,
+    widthSegments: 1,
+    heightSegments: 1,
+    depthSegments: 1,
+    rotateX: 0,
+    rotateY: 0,
+    rotateZ: 0, 
+    positionX: 0,
+    positionY: 0,
+    positionZ: 0
+};
+
+fetch("https://ghcdn.rawgit.org/sainitripti/visualiser/master/data/box.json")
+.then(response => {
+   return response.json();
+})
+.then(data => {
+  boxData = data["boxData"];   
+  updateGeometries();
+});
+
+var axial = new LessonOnePreviewCard("axial-container", "axial-gui-container", "0", "70", "axial");
+var sagittal = new LessonOnePreviewCard("sagittal-container", "sagittal-gui-container", "1", "50", "sagittal");
+var coronal = new LessonOnePreviewCard("coronal-container", "coronal-gui-container", "2", "55", "coronal");
+
+function updateGeometries()
+{
+  axial.generateGeometry(boxData);
+  sagittal.generateGeometry(boxData);
+  coronal.generateGeometry(boxData);
+}
+
+function render()
+{ 
+  axial.run();
+  sagittal.run();
+  coronal.run();
+}
+
+
+updateGeometries();
+render();
+/*
+const boxId = "l1";
+var boxGeometryPanel = new BoxGeometryPanel(boxData,boxId);
+boxGeometryPanel.render();
+
+function handleBoxGeometryChange(e)
+{
+  // Remove boxId appended at end of each Id
+  var property = e.target.id.slice(0,-boxId.length);
+  //Update boxData
+  boxData[property] = e.target.value;
+  updateGeometries();
+}
+
+const boxGeometryL1 = document.getElementById("boxgeometry"+boxId);
+const properties = ["width","height","depth", "rotateX", "rotateY", "rotateZ", "positionX", "positionY", "positionZ"];
+properties.forEach(property => document.getElementById(property+boxId)
+  .addEventListener("change", handleBoxGeometryChange));
+*/
